92964d4d6347179c3bf4e8b8d49e588492f80275
Chris Dzombak <chris@dzombak.com>
2021-10-08 10:42:14 -0400

    Update basic WiFi connection + blinking demo

---
+++ src/led.cpp
@@ -0,0 +1,58 @@
+#include <Arduino.h>
+#include "led.h"
+
+// nb. Wemos D1 Mini onboard LED is active-low
+
+volatile bool ledState; // the current LED state
+bool ledBlinkEnabled; // whether blinking has started (ie. whether the ISR is attached)
+uint32_t ledCountsOff, ledCountsOn; // desired off/on times, in timer counts of 3.2us
+
+IRAM_ATTR void ledTimerISR() {
+    if (ledState) {
+        ledState = false;
+        digitalWrite(LED_BUILTIN, HIGH);
+        timer1_write(ledCountsOff);
+        timer1_enable(TIM_DIV256, TIM_EDGE, TIM_SINGLE);
+    } else {
+        ledState = true;
+        digitalWrite(LED_BUILTIN, LOW);
+        timer1_write(ledCountsOn);
+        timer1_enable(TIM_DIV256, TIM_EDGE, TIM_SINGLE);
+    }
+}
+
+void blinkLED(unsigned long desiredTimeOn, unsigned long desiredTimeOff) {
+    // TIM_DIV256 = 3 //312.5Khz (1 tick = 3.2us - 26843542.4 us max)
+    // milliseconds * 1000 = microseconds
+    // microseconds / 3.2 = ticks
+    // => milliseconds * (1000/3.2) = ticks
+    // => milliseconds * 312.5 = ticks
+    // ... and then we actually use 312, to avoid floating-point math entirely
+    auto desiredCountsOn = desiredTimeOn * 312;
+    auto desiredCountsOff = desiredTimeOff * 312;
+    if (!ledBlinkEnabled || desiredCountsOff != ledCountsOff || desiredCountsOn != ledCountsOn) {
+        ledCountsOn = desiredCountsOn;
+        ledCountsOff = desiredCountsOff;
+        ledState = true;
+        digitalWrite(LED_BUILTIN, LOW);
+        if (ledBlinkEnabled) {
+            timer1_disable();
+            timer1_write(ledCountsOn);
+            timer1_enable(TIM_DIV256, TIM_EDGE, TIM_SINGLE);
+        } else {
+            ledBlinkEnabled = true;
+            pinMode(LED_BUILTIN, OUTPUT);
+            timer1_attachInterrupt(ledTimerISR);
+            timer1_write(ledCountsOn);
+            timer1_enable(TIM_DIV256, TIM_EDGE, TIM_SINGLE);
+        }
+    }
+}
+
+void stopLED() {
+    if (ledBlinkEnabled) {
+        timer1_disable();
+        timer1_detachInterrupt();
+        ledBlinkEnabled = false;
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
-#include <AsyncPing.h>
+#include <ESP8266WiFi.h>    // https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/readme.html
+#include <ESP8266mDNS.h>    // https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266mDNS
+#include <ESP8266Ping.h>    // https://github.com/dancol90/ESP8266Ping
 
 #define _TASK_SLEEP_ON_IDLE_RUN
 #include <TaskScheduler.h>
 
+#include "led.h"
 #include "config.h"  // git-ignored file for configuration including secrets
 
 // Goal here is to:
@@ -21,46 +22,31 @@ Scheduler ts;
 
 // Callback method prototypes
 void connectInit();
-void ledCallback();
-bool onLedEnable();
-void onLedDisable();
-void ledOn();
-void ledOff();
 bool onMDNSEnable();
 void mDNSCallback();
 void connMonitorCallback();
 void pingCallback();
 
 // Ping (our "useful work")
-AsyncPing ping;
 #define PING_COUNT         (2)
 #define PING_TIMEOUT       (TASK_SECOND/3)
-#define PING_TASK_INTERVAL (PING_TIMEOUT*PING_COUNT + TASK_MILLISECOND*100)
-//#define PING_PRINT_SUMMARY
+#define PING_TASK_INTERVAL (PING_TIMEOUT*PING_COUNT + TASK_MILLISECOND*500)
+#define PING_TARGET        IPAddress(8, 8, 4, 4)
 
 // Tasks
 Task  tConnect     (TASK_SECOND, TASK_FOREVER, &connectInit, &ts, true);  // handles waiting on initial WiFi connection
-Task  tLED         (TASK_IMMEDIATE, TASK_FOREVER, &ledCallback, &ts, false, &onLedEnable, &onLedDisable);
 Task  tMDNS        (TASK_MILLISECOND*50, TASK_FOREVER, &mDNSCallback, &ts, false, &onMDNSEnable, nullptr);
 Task  tConnMonitor (TASK_SECOND, TASK_FOREVER, &connMonitorCallback, &ts, false);
 Task  tPing        (PING_TASK_INTERVAL, TASK_FOREVER, &pingCallback, &ts, false);  // simulates starting our "useful work"
 
-// WiFi
-const char *ssid     = CFG_WIFI_ESSID;
-const char *pwd      = CFG_WIFI_PASSWORD;
-const char *hostname = CFG_HOSTNAME;
-
-// LED
-bool ledState;
-long ledTimeOff, ledTimeOn;
-#define CONNECTED_LED_TIME_ON   (TASK_SECOND/3)
-#define CONNECTED_LED_TIME_OFF  (TASK_SECOND)
-#define CONNECTING_LED_TIME_ON  (TASK_SECOND/4)
-#define CONNECTING_LED_TIME_OFF (TASK_SECOND/4)
+// LED (all times in milliseconds)
+#define CONNECTED_LED_TIME_ON   (TASK_SECOND/5)
+#define CONNECTED_LED_TIME_OFF  (TASK_SECOND*1.5)
+#define CONNECTING_LED_TIME_ON  (TASK_SECOND/5)
+#define CONNECTING_LED_TIME_OFF CONNECTING_LED_TIME_ON
 
 void setup() {
     Serial.begin(SERIAL_BAUD);
-    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
@@ -71,17 +57,12 @@ void loop() {
    Wait for initial WiFi connection
 */
 void connectWait() {
-    Serial.print(millis());
-    Serial.println(F(": Waiting for initial connection"));
+    Serial.printf_P(PSTR("%lu: Waiting for initial WiFi connection\r\n"), millis());
 
     if (WiFi.status() == WL_CONNECTED) {
-        Serial.print(millis());
-        Serial.print(F(": Connected. My IP: "));
-        Serial.println(WiFi.localIP());
-        ledTimeOff = CONNECTED_LED_TIME_OFF;
-        ledTimeOn = CONNECTED_LED_TIME_ON;
-        tLED.enableIfNot();
-        tConnect.disable();  // triggers internal status signal to resolve to 0
+        Serial.printf_P(PSTR("%lu: Connected. My IP: %s\r\n"), millis(), WiFi.localIP().toString().c_str());
+        blinkLED(CONNECTED_LED_TIME_ON, CONNECTED_LED_TIME_OFF);
+        tConnect.disable();
 
         tMDNS.enable();
         tConnMonitor.enable();
@@ -92,19 +73,12 @@ void connectWait() {
    Initiate connection to the WiFi network
 */
 void connectInit() {
-    Serial.print(F("Connecting to WiFi ("));
-    Serial.print(ssid);
-    Serial.println(F(")"));
-
+    Serial.printf_P(PSTR("%lu: Connecting to WiFi (%s)\r\n"), millis(), CFG_WIFI_ESSID);
     WiFi.mode(WIFI_STA);
-    WiFi.hostname(hostname);
-    WiFi.begin(ssid, pwd);
+    WiFi.hostname(CFG_HOSTNAME);
+    WiFi.begin(CFG_WIFI_ESSID, CFG_WIFI_PASSWORD);
+    blinkLED(CONNECTING_LED_TIME_ON, CONNECTING_LED_TIME_OFF);
     yield();  // esp8266 yield allows WiFi stack to run
-
-    ledTimeOff = CONNECTING_LED_TIME_OFF;
-    ledTimeOn = CONNECTING_LED_TIME_ON;
-    tLED.enable();
-
     tConnect.yield(&connectWait);  // pass control to Scheduler; wait for initial WiFi connection
 }
 
@@ -112,7 +86,7 @@ void connectInit() {
  * Start the mDNS stack.
  */
 bool onMDNSEnable() {
-    return MDNS.begin(hostname);
+    return MDNS.begin(CFG_HOSTNAME);
 }
 
 /**
@@ -130,15 +104,11 @@ void mDNSCallback() {
 void connMonitorCallback() {
     auto status = WiFi.status();
     if (status == WL_CONNECTED) {
-        ledTimeOff = CONNECTED_LED_TIME_OFF;
-        ledTimeOn = CONNECTED_LED_TIME_ON;
-        tLED.enableIfNot();
+        blinkLED(CONNECTED_LED_TIME_ON, CONNECTED_LED_TIME_OFF);
         tPing.enableIfNot();
     } else {
         Serial.print(millis()); Serial.print(F(": WiFi connection status: ")); Serial.println(status);
-        ledTimeOff = CONNECTING_LED_TIME_OFF;
-        ledTimeOn = CONNECTING_LED_TIME_ON;
-        tLED.enableIfNot();
+        blinkLED(CONNECTING_LED_TIME_ON, CONNECTING_LED_TIME_OFF);
         tPing.disable();
     }
 }
@@ -148,64 +118,10 @@ void connMonitorCallback() {
  * This simulates the "useful work" this project performs.
  */
 void pingCallback() {
-    // adapted from sample code in https://github.com/akaJes/AsyncPing README.
-
-    ping.on(true,[](const AsyncPingResponse& response){
-        IPAddress addr(response.addr); // to prevent with no const toString() in 2.3.0
-        if (response.answer)
-            Serial.printf_P(PSTR("%lu: %d bytes from %s: icmp_seq=%d ttl=%d time=%lu ms\n"), millis(), response.size, addr.toString().c_str(), response.icmp_seq, response.ttl, response.time);
-        else
-            Serial.printf_P(PSTR("%lu: no answer yet for %s icmp_seq=%d\n"), millis(), addr.toString().c_str(), response.icmp_seq);
-        return false; // do not stop
-    });
-    ping.on(false,[](const AsyncPingResponse& response){
-        IPAddress addr(response.addr); // to prevent with no const toString() in 2.3.0
-        Serial.printf_P(PSTR("%lu: total answer from %s sent %d recevied %d time %lu ms\n"), millis(), addr.toString().c_str(),response.total_sent,response.total_recv,response.total_time);
-        return true;
-    });
-
-    ping.begin(IPAddress(8, 8, 4, 4), PING_COUNT, PING_TIMEOUT);
-}
-
-/**
- * Flip the LED state based on the current state.
- */
-void ledCallback() {
-    if (ledState) ledOff();
-    else ledOn();
-}
-
-/**
- * Make sure the LED starts lit.
- */
-bool onLedEnable() {
-    ledOn();
-    return true;
-}
-
-/**
- * Make sure LED ends dimmed.
- */
-void onLedDisable() {
-    ledOff();
-}
-
-/**
- * Turn LED on; set delay to ledTimeOn.
- * nb. Wemos D1 Mini onboard LED is active-low
- */
-void ledOn() {
-    ledState = true;
-    digitalWrite(LED_BUILTIN, LOW);
-    tLED.delay(ledTimeOn);
-}
-
-/**
- * Turn LED off; set delay to ledTimeOff.
- * nb. Wemos D1 Mini onboard LED is active-low
- */
-void ledOff() {
-    ledState = false;
-    digitalWrite(LED_BUILTIN, HIGH);
-    tLED.delay(ledTimeOff);
+    bool pingSuccess = Ping.ping(PING_TARGET, PING_COUNT);
+    if (pingSuccess) {
+        Serial.printf_P(PSTR("%lu: ping success; avg time %dms\r\n"), millis(), Ping.averageTime());
+    } else {
+        Serial.printf_P(PSTR("%lu: ping failed\r\n"), millis());
+    }
 }
